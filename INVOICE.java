07c0f331b0e62bb7c4ffd3f45f9bc409779d3282
leetmartins <108501966+leetmartins@users.noreply.github.com>
2022-11-17 17:44:45 -0300

    Update INVOICE.java

--- INVOICE.java
+++ INVOICE.java
@@ -1,1 +1,29 @@
-public class INVOICE {	public static void main(String[] args) {	   	   String state[] = {"SP","RJ","MG","ES","Others"};	   double partial [] = {67836.43, 36678.66, 29229.88, 27165.48, 19849.53}, total = 0;	   	   for(int i = 0; i < 5; i++){	       	       total += partial[i];	   }	   	   	   percent(state,partial,total); 	   	}		public static void percent(String[] state, double[] partial, double total){	    	    for(int i = 0; i < 5; i++) {    	       	    double element = (partial[i] / total) * 100;    	        	    System.out.printf("State: " + state[i] + "        |   " + "Percent: " + element);    	    System.out.println();	    }	    	    	}}
+public class INVOICE {
+	public static void main(String[] args) {
+	   
+	   String state[] = {"SP","RJ","MG","ES","Others"};
+	   double partial [] = {67836.43, 36678.66, 29229.88, 27165.48, 19849.53}, total = 0;
+	   
+	   for(int i = 0; i < 5; i++){
+	       
+	       total += partial[i];
+	   }
+	   
+	   
+	   percent(state,partial,total); 
+	   
+	}
+	
+	public static void percent(String[] state, double[] partial, double total){
+	    
+	    for(int i = 0; i < 5; i++) {
+    	   
+    	    double element = (partial[i] / total) * 100;
+    	    
+    	    System.out.printf("State: " + state[i] + "        |   " + "Percent: " + element);
+    	    System.out.println();
+	    }
+	    
+	    
+	}
+}
